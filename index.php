0270f3b1504f0ac46d64e487dc6228aeac025efc
reevr <reev.r00@gmail.com>
2017-09-19 17:03:33 +0530

    index page design updated

--- index.php
+++ index.php
@@ -34,28 +34,18 @@ session_start();
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-        <style>
-        .search_box {
-            height:auto;
-            margin-top:200px;
-            padding : 20px;
-        }
-
-        .submit_button {
-            margin : 10px 42%;
-        }
-        </style>
+        <link href="css/main.css" rel="stylesheet">
     </head>
 <body>
 <div class="nav navbar-inverse">
             <div class="container-fluid">
                 <div class="navbar-header">
-                    <a class="navbar-brand">Region Survey</a>
+                    <li><a class="navbar-brand">RentalSurveyCompany</a></li>
                 </div>
-                <ul class="nav navbar-nav">
+                <ul class="nav navbar-nav navbar-right">
                     <li class="active"><a href="index.php">Home</a></li>
                     <li><a href="surveyEntry.php">Survey Entry</a></li>
-                    <li><a href="surveyResult.php">Survey Result</a></li>
+                    <!--<li><a href="surveyResult.php">Survey Result</a></li>-->
                     <li><a href="<?php echo $currentURL. '?stat=true'; ?>">Get statics</a></li>
                 </ul> 
             </div>
@@ -67,15 +57,16 @@ session_start();
         }
 
         ?>
+<div class="jumbotron heading text-center">
+        <h1>Find your dream property.</h1>
+        <h3> Every information at your finger tip. </h3>
+</div>
  <div class="container">
        <div class="row">
-            <div class="col-md-offset-3 col-md-7 col-md-offset-3">
+            <div class="col-md-offset-2 col-md-8 col-md-offset-2">
             <form class="search_box" action="index.php">
-                <div class="form-group">
-                    <label> Enter Location </label>
-                    <input type="text" class="form-control" name="searchLocation" >
-                    <input type="submit" style="display:none;" >
-                </div>
+                     <input type="text" class="form-control" name="searchLocation" >
+                     <a href="#" class="btn btn-info btn-lg"><span class="glyphicon glyphicon-search"></span> </a>
             </form>
             <?php
                     session_start(); 
